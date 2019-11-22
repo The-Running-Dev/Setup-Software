1ced42301f8249e8024fa44ca6009f8da1df3ea1
The Running Dev <therunningdev@gmail.com>
2019-11-22 13:56:52 +0100

    Added instructions and status updates

--- VisualStudio/VisualStudio.Community.ps1
+++ VisualStudio/VisualStudio.Community.ps1
@@ -4,15 +4,31 @@ $configUrl = 'https://github.com/The-Running-Dev/Setup-Software/raw/master/Visua
 $installerPath = Join-Path $env:Temp 'vs_community.exe'
 $configPath = Join-Path $env:Temp '.vsconfig'
 
+"
+You are Installing Visual Studio Community With:
+
+.NET Desktop Development
+.NET Core Cross-Platform Development
+ASP.NET and Web Development
+Mobile Development with .NET
+" | Write-Output
+
+Read-Host "Hit Any Key to Continue..."
+
 # Download the installer
+"Downloading Installer '$downloadUrl' to '$installerPath'..." | Write-Output
 (New-Object System.Net.WebClient).DownloadFile($downloadUrl, $installerPath)
 
 # Download the config file
+"Downloading Config '$configUrl' to '$configPath'..." | Write-Output
 (New-Object System.Net.WebClient).DownloadFile($configUrl, $configPath)
 
 if ((Test-Path $installer) -and (Test-Path $configPath)) {
+    "Running Installer '$installer'
+    Arguments: --config ""$configPath"" --passive --norestart --wait" | Write-Output
+
     Start-Process `
         $installer `
-        "--config \"$configPath\" --passive --norestart --wait" `
+        "--config ""$configPath"" --passive --norestart --wait" `
         -Wait
 }
